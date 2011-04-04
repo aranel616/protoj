9acf125bfd6bc4ed7311c05ba12c57abe8553b5a
Michael Day <manveru.alma@gmail.com>
2011-04-04 11:50:22 +0000

    Completing 31976.fade and 31976.show

--- protoj.js
+++ protoj.js
@@ -1,6 +1,6 @@
-delete window.$$;
+delete window.$$$;
 
-window.$$ = function() {
+window.$$$ = function() {
   var expression = $A(arguments).join(', ');
   return new protoj(Prototype.Selector.select(expression, document));
 };
@@ -12,17 +12,69 @@ protoj = function(data) {
 	return data;
 };
 
-protoj.prototype.hide = function() {
+protoj.speeds = {
+	fast: 200,
+	slow: 600,
+	_default: 400
+};
+
+protoj.prototype.hide = function(speed, callback) {
+	/* Allow callback to be the first and only parameter */
+	if (Object.isFunction(speed))
+		callback = speed;
+	
+	/* Set speed if value is 'fast' or 'slow' */ 
+	if (protoj.speeds[speed])
+		speed = protoj.speeds[speed];
+	else if (!Object.isNumber(speed))
+		speed = protoj.speeds['_default'];
+	
 	this.each(function(e){
-		e.hide();
+		if (speed == 0) {
+			e.hide();
+			
+			if (typeof callback == 'function')
+				callback.call(e);
+		} else {
+			Effect.Fade(e, {
+				duration: speed / 1000,
+				afterFinish: function(){
+					if (typeof callback == 'function')
+						callback.call(e);
+				}
+			});
+		}
 	})
 	
 	return this;
 }
 
-protoj.prototype.show = function() {
+protoj.prototype.show = function(speed, callback) {
+	/* Allow callback to be the first and only parameter */
+	if (Object.isFunction(speed))
+		callback = speed;
+	
+	/* Set speed if value is 'fast' or 'slow' */ 
+	if (protoj.speeds[speed])
+		speed = protoj.speeds[speed];
+	else if (!Object.isNumber(speed))
+		speed = protoj.speeds['_default'];
+	
 	this.each(function(e){
-		e.show();
+		if (speed == 0) {
+			e.show();
+			
+			if (typeof callback == 'function')
+				callback.call(e);
+		} else {
+			Effect.Appear(e, {
+				duration: speed / 1000,
+				afterFinish: function(){
+					if (typeof callback == 'function')
+						callback.call(e);
+				}
+			});
+		}
 	})
 	
 	return this;
